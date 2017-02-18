266d18e057bdb2147692ea54f4a1b2e0a6ac2583
Peter Schuller <peter.schuller@infidyne.com>
2017-02-18 10:48:24 -0800

    Initial commit.

---
+++ gogctest.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hashicorp/golang-lru"
+	"time"
+)
+
+const (
+	LRU_SIZE                      = 10000000
+	PAUSE_REPORT_THRESHOLD_MILLIS = 1
+	SLOW_PHASE_THRESHOLD          = LRU_SIZE * 2
+	PAUSE_DURATION                = "5ms"
+	PAUSE_INTERVAL                = 1000
+)
+
+func main() {
+	l, _ := lru.New(LRU_SIZE)
+	d, _ := time.ParseDuration(PAUSE_DURATION)
+
+	fmt.Println("filling lru")
+	for i := uint64(0); i < (1 << 63); i++ {
+		startTime := time.Now().UnixNano()
+		l.Add(i, fmt.Sprintf("val%s", i))
+		stopTime := time.Now().UnixNano()
+
+		elapsedMillis := (stopTime - startTime) / 1000000
+
+		if elapsedMillis > PAUSE_REPORT_THRESHOLD_MILLIS {
+			fmt.Printf("lru add took %v ms\n", elapsedMillis)
+		}
+
+		if i == LRU_SIZE {
+			fmt.Println("lru filled")
+		}
+
+		if i == SLOW_PHASE_THRESHOLD {
+			fmt.Println("slow phase starting")
+		}
+
+		if i > SLOW_PHASE_THRESHOLD && i%PAUSE_INTERVAL == 0 {
+			time.Sleep(d)
+		}
+	}
+}
