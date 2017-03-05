b223f465a558e6ad7ee81a6dd37d2cd59a634c03
Peter Schuller <peter.schuller@infidyne.com>
2017-03-05 00:05:46 -0800

    Allow setting LRU size and add rate at cmdline.
    These are the two that must be varied based on the host environment,
    so most important to allow easy modification without rebuild.

--- gogctest.go
+++ gogctest.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"github.com/hashicorp/golang-lru"
 	"golang.org/x/time/rate"
@@ -14,10 +15,9 @@ import (
 const (
 	THREAD_COUNT = 1000
 	// Total size of LRU across all threads.
-	TOTAL_LRU_SIZE = 10000000
+	DEFAULT_TOTAL_LRU_SIZE = 10000000
 	// Total rate of LRU adds across all threads.
-	TOTAL_ADD_RATE = 10000
-	ADD_RATE_BURST = TOTAL_ADD_RATE / 10
+	DEFAULT_TOTAL_ADD_RATE = 10000
 
 	PAUSE_REPORT_THRESHOLD         = "1ms"
 	HICCUP_DETECTOR_SLEEP_DURATION = "1ms"
@@ -29,7 +29,23 @@ const (
 	STARTUP_DELAY = "5s"
 )
 
+var totalAddRate float64
+var addRateBurst int
+var totalLruSize int
+
 func main() {
+	flag.Float64Var(&totalAddRate, "addrate", DEFAULT_TOTAL_ADD_RATE, "Number of LRU adds per second.")
+	flag.IntVar(&totalLruSize, "lrusize", DEFAULT_TOTAL_LRU_SIZE, "LRU size (number of entries)")
+	flag.Parse()
+
+	// LRU implementation panics on use if size is 0, so don't allow sizes smaller than
+	// thread count since that would lead to 0 per thread.
+	if totalLruSize < THREAD_COUNT {
+		log.Fatal("lru size must be >= thread count")
+	}
+
+	addRateBurst = int(totalAddRate/THREAD_COUNT) / 10
+
 	wg := sync.WaitGroup{}
 
 	wg.Add(1)
@@ -52,9 +68,9 @@ func lruWorker() {
 		float64(startupDelay.Nanoseconds())*
 			rand.Float64())))
 
-	lruSize := TOTAL_LRU_SIZE / THREAD_COUNT
+	lruSize := totalLruSize / THREAD_COUNT
 	l, _ := lru.New(lruSize)
-	rateLimiter := rate.NewLimiter(TOTAL_ADD_RATE/THREAD_COUNT, ADD_RATE_BURST)
+	rateLimiter := rate.NewLimiter(rate.Limit(float64(totalAddRate)/float64(THREAD_COUNT)), addRateBurst)
 	pauseReportThreshold, err := time.ParseDuration(PAUSE_REPORT_THRESHOLD)
 	if err != nil {
 		log.Panic("couldn't parse duration")
